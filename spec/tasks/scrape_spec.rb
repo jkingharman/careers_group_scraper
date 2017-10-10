a3f34bad67b3f5d8afd5fa0b8cf2d149e1e860b1
Jasper King-Harman <j.kingharman@googlemail.com>
2017-10-10 13:07:02 +0100

    Add basic rake test - return to test for args

--- spec/tasks/scrape_spec.rb
+++ spec/tasks/scrape_spec.rb
@@ -1,3 +1,29 @@
 
+
 require 'spec_helper'
 require 'rake'
+
+  describe 'scrape:vacencies_to_json' do
+
+    # setting up context for tests
+    let(:rake)  { Rake::Application.new }
+    subject { Rake.application.tasks[1] }
+
+    before do
+      Rake.application.rake_require "tasks/scrape"
+      Rake::Task.define_task(:environment)
+    end
+
+    let(:scraper_job) { double(:scraper_job, perform: nil) }
+
+    before do
+      allow(ScraperJob).to receive(:new).and_return(scraper_job)
+      allow(scraper_job).to receive(:perform)
+    end
+
+    it 'performs the scraper job' do
+      expect(scraper_job).to receive(:perform)
+      subject.invoke
+    end
+
+end
